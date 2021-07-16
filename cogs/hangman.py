b517dc280f9830a6fc7299c402fecaa8d90895b9
Ang Jun Ray <angjunray@live.com>
2021-07-16 22:17:27 +0800

    hangman and quiz (math) done

---
+++ cogs/hangman.py
@@ -0,0 +1,192 @@
+from bs4 import BeautifulSoup as soup
+import os
+from urllib.request import urlopen as uReq
+import bs4
+import time
+import discord
+from discord.ext import commands
+
+from urllib.request import urlopen as uReq
+from bs4 import BeautifulSoup as soup
+import os
+import sys
+import time
+
+
+class Hangman(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command()
+    async def hangman(self, ctx):
+        myList = []
+        words = open("../DataBase/words.txt", "r")
+        for topic in words:
+            myList.append(topic.rstrip("\n"))
+        words.close() #can we run it?
+    #     while True: 
+    #         topic = input(
+    #          "Wh
+    #          at topic do you want to choose? Type ? for list of topics!")
+    #      if topic == "?":
+    #          print("----------LIST OF TOPICS AVALIABLE------------")
+    #          for word in myList:
+    #              print(word)
+    #          print("------------------END OF LIST-----------------")
+    #      elif topic in myList:
+    #          break
+    #      else:
+    #          Functions.error(
+    #              topic,
+    #              "Input must be in the list of topics. Input ? for list! (Please check if ur caps differs. It matters!)",
+    #          )
+      
+    #   generatedWord = Functions.generateWord("", topic)
+      
+    #   shownToUser = []
+    #   for i in generatedWord:
+    #      shownToUser.append("_")
+    #   word = list(generatedWord)
+    #   guessedletters = 0  # incldues duplicates
+    #   guessedAlph = []
+    #   lives = 5
+    #   print("You have 6 lives, each incorrect guess is 1 live subtracted")
+      
+    #   Functions.enumChecker(7)
+      
+    #   while guessedletters < len(generatedWord):
+    #      result = Functions.move(word, guessedAlph, lives, shownToUser)
+    #      guessedAlph.append(result[1])
+    #      guessedletters += int(result[0])
+    #      lives = int(result[2])
+      
+    #      counter = 0
+    #      for i in generatedWord:
+    #          if result[1] == i:
+    #              shownToUser[counter] = result[1]
+    #          counter += 1
+      
+    #      if lives == 0:
+    #          print(
+    #              f"Oh no, u have lost all your lives :( THe word was {generatedWord}. Try again next time!"
+    #          )
+    #          break
+    #      if "_" not in shownToUser:
+    #          print("You have won! Good job :D")
+    #          break
+    #      time.sleep(2)
+      
+    #   Functions.enumChecker(lives + 1)
+      
+    #   Functions.separationLine()
+    #   Functions.separationLine()
+      
+      
+    #   print(
+    #      """The word database is taken from enchantedlearning.com.
+    #   Hangman ASCII pictures are taken from https://gist.github.com/chrishorton/8510732aa9a80a03c829b09f12e20d9c.
+    #   Thanks for playing! More features coming soon!"""
+    #   )
+      
+    #   Functions.separationLine()
+      
+    #   # --------------------
+    #   # end of program
+    #   # --------------------
+    #   endtime = time.time()
+    #   totaltime = startime - endtime
+    #   totaltime = str(abs(round(totaltime / 60, 2)))
+    #   print(f"You took {totaltime} minutes to complete the Hangman word program!")
+    #   time.sleep(10)
+      
+    #   pass
+
+
+def setup(client):
+    client.add_cog(Hangman(client))
+
+
+# # --------GET HANGMAN FILES------------
+# starttime = time.time()
+
+# # --------FIRST PARSE OF LINKS----------
+# my_url = "https://www.enchantedlearning.com/wordlist"
+# uClient = uReq(my_url)
+# page_html = uClient.read()
+# uClient.close()
+
+# page = soup(page_html, "html.parser")
+
+# table = page.select(
+#     "body > p:nth-child(13) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1)"
+# )
+# tables = page.find_all("td")
+
+# counter = 12
+# links = []
+# topic = []
+
+
+# while counter <= 15:
+#     container = tables[counter].find_all("a")
+#     for label in container:
+#         links.append(label.get("href"))
+#         topic.append(label.text.replace("/", "").replace('"', ""))
+#     counter += 1
+
+# # ---------SECOND PARSE OF WORDS------------
+# numberOfWords = 0
+# hallo = input(
+#     """Do you have a folder named 'Database' with files inside already?
+# If yes, input 1(safer option if this is ur first time running this), if not, input anything else
+# WARNING: IF THERE IS NO FILES REQUIRED, it might crash!"""
+# )
+
+# if hallo == "1":
+#     a = input(
+#         """Is it alright if we create a few files showing the databank of words for this?
+# WARNING: This takes > 3.35 minutes, depending on internet speed and CPU
+# If you think you do not have it (checking system to see if u already have it or no coming up), please input 1. Otherwise, input anything. """
+#     )
+
+#     if a == "1":
+#         while True:
+#             try:
+#                 os.chdir(str(os.getcwd()) + "../DataBase")
+#                 break
+#             except:
+#                 print("U do not have a Database, creating it now...")
+#                 try:
+#                     directory = "DataBase"
+#                     parent_dir = os.getcwd()
+#                     path = os.path.join(parent_dir, directory)
+#                     os.mkdir(path)
+#                     os.chdir(str(os.getcwd()) + "/DataBase")
+#                     break
+#                 except:
+#                     print("Failed to make database, please report error to github")
+
+#         print("Making files in " + str(os.getcwd()) + " ")
+
+#         counter1 = 0
+
+#         for i in topic:
+#             with open(i, "w+") as file1:
+#                 website = "https://www.enchantedlearning.com" + links[counter1]
+#                 uClient = uReq(website)
+#                 page_html = uClient.read()
+#                 uClient.close()
+#                 page = soup(page_html, "html.parser")
+#                 for word in page.find_all("div", {"class": "wordlist-item"}):
+#                     file1.write(word.text)
+#                     file1.write("\n")
+#                     numberOfWords += 1
+#             counter1 += 1
+#             print(f"Made file with topic {topic[counter1-1]}")
+
+#         endtime = time.time()
+#         totaltime = str(abs(round(starttime - endtime, 5) / 60))
+#         print(
+#             f"It took {totaltime} minutes to run the program! A total of {numberOfWords} words has been added to the database"
+#         )
+#         os.chdir("..")
--- cogs/quiz.py
+++ cogs/quiz.py
@@ -8,8 +8,11 @@ import asyncio
 import firebase_admin
 from firebase_admin import firestore
 
-questions = [""]
+guess_questions = ["What is 1+1?", ""]
+guess_answers = ["2"]
 alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+
 class Quiz(commands.Cog):
     def __init__(self, client):
         self.client = client
@@ -18,7 +21,7 @@ class Quiz(commands.Cog):
 
     # @cooldown(1, 20, BucketType.user)
     @commands.command()
-    async def trivia(self, ctx, difficulty = 100000000000):
+    async def trivia(self, ctx, difficulty=100000000000):
         word = "hard"
         moneyToAdd = 0
         if(difficulty == 1):
@@ -31,36 +34,38 @@ class Quiz(commands.Cog):
             word = "hard"
             moneyToAdd = 10
         else:
-            await ctx.reply(embed=discord.Embed(title = "Error", description="A difficulty level of 1, 2 or 3 is needed! Note that the harder the question is, the more points you will get if u get it right!", colour=self.client.primary_colour))
+            await ctx.reply(embed=discord.Embed(title="Error", description="A difficulty level of 1, 2 or 3 is needed! Note that the harder the question is, the more points you will get if u get it right!", colour=self.client.primary_colour))
             return
         r = {
-            "response_code":'value1',
-            "results":[
+            "response_code": 'value1',
+            "results": [
                 {
-                    "category":'value2',
-                    "type":'value3',
-                    "difficulty":'value4',
-                    "question":'value5',
-                    "correct_answer":'value6',
-                    "incorrect_answers":'value7'
+                    "category": 'value2',
+                    "type": 'value3',
+                    "difficulty": 'value4',
+                    "question": 'value5',
+                    "correct_answer": 'value6',
+                    "incorrect_answers": 'value7'
                 }
-                ]
-            }
-        result = json.loads(requests.post(f"https://opentdb.com/api.php?amount=1&difficulty={word}&type=multiple", data=r).text)
+            ]
+        }
+        result = json.loads(requests.post(
+            f"https://opentdb.com/api.php?amount=1&difficulty={word}&type=multiple", data=r).text)
         results = result["results"][0]
-        
+
         arr = results["incorrect_answers"]
-        arr.append(str(results["correct_answer"])) 
+        arr.append(str(results["correct_answer"]))
         random.shuffle(arr)
         count = 0
-        description=""
+        description = ""
         for i in arr:
             description += f'({count+1}) {i}\n'
             count += 1
         embed = discord.Embed(
-            title= f"TRIVIA- You have 10 seconds to answer ({word})",
-            description=results["question"].replace("&quot;", '\"').replace("&#039;","\'")+'\n'+description,
-        
+            title=f"TRIVIA- You have 10 seconds to answer ({word})",
+            description=results["question"].replace(
+                "&quot;", '\"').replace("&#039;", "\'")+'\n'+description,
+
             colour=self.client.primary_colour
         )
         message = await ctx.reply(embed=embed)
@@ -69,6 +74,7 @@ class Quiz(commands.Cog):
         await message.add_reaction("3️⃣")
         await message.add_reaction("4️⃣")
         # res = await Bot.wait_for_reaction('3️⃣', message=msg, timeout=15)
+
         def check(reaction, user):
             return user == ctx.author and reaction.message == message
 
@@ -76,14 +82,14 @@ class Quiz(commands.Cog):
             reaction, user = await self.client.wait_for('reaction_add', timeout=10.0, check=check)
         except asyncio.TimeoutError:
             embed = discord.Embed(
-                title = "You were too slow! Try again next time.",
-                description = " ",
-                color = self.client.primary_colour
+                title="You were too slow! Try again next time.",
+                description=" ",
+                color=self.client.primary_colour
             )
             await ctx.reply(embed=embed)
-            
+
         else:
-            #find which option is correct????
+            # find which option is correct????
             index = None
             if reaction.emoji == '1️⃣':
                 index = 0
@@ -103,29 +109,107 @@ class Quiz(commands.Cog):
             if doc.exists:
                 dict1 = doc.to_dict()
                 if ans == str(results["correct_answer"]):
-                    dict1["money"] = dict1["money"] + random.randint(0, moneyToAdd)
+                    dict1["money"] = dict1["money"] + \
+                        random.randint(0, moneyToAdd)
                     embed = discord.Embed(
-                        title = "Correct Answer! You win 3 money!",
-                        description = "You now have " + str(dict1["money"]) + " money!",
-                        color = self.client.primary_colour
+                        title="Correct Answer! You win 3 money!",
+                        description="You now have " +
+                        str(dict1["money"]) + " money!",
+                        color=self.client.primary_colour
                     )
                     await ctx.reply(embed=embed)
-                    
+
                 else:
-                    
-                    dict1["money"] = dict1["money"] - int(random.randint(0, moneyToAdd)/3)
+
+                    dict1["money"] = dict1["money"] - \
+                        int(random.randint(0, moneyToAdd)/3)
                     embed = discord.Embed(
-                        title = "Wrong Answer! You lost 1 money!",
-                        description = f"You now have " + str(dict1["money"]) + f" money! The correct answer was {results['correct_answer']} :",
-                        color = self.client.primary_colour
+                        title="Wrong Answer! You lost 1 money!",
+                        description=f"You now have " +
+                        str(dict1["money"]) +
+                        f" money! The correct answer was {results['correct_answer']} :",
+                        color=self.client.primary_colour
                     )
                     await ctx.reply(embed=embed)
                 doc_ref.set(dict1)
 
+    @commands.command()
+    async def math(self, ctx):
+        first = random.randint(1, 100)
+        second = random.randint(1, 100)
+        operandation = random.randint(1, 100)
+        oper = "+"
+        if operandation < 9:
+            oper = "*"
+            theanswer = str(first*second)
+            timehehe = 10 + (first+second-69)/20
+        elif operandation < 40:
+            oper = "-"
+            theanswer = str(first-second)
+            timehehe = 6 + (first+second-49)/30
+        else:
+            timehehe = 4 + (first+second-69)/60
+            theanswer = str(first+second)
+        timehehe = int(timehehe)
+
+        question_ask = "What is " + str(first) + oper + str(second) + "?"
+
+        embed = discord.Embed(
+            title=question_ask,
+
+            description="You have " +
+            str(timehehe) + " seconds to answer! You only have one chance.",
+            color=self.client.primary_colour
+        )
+        await ctx.reply(embed=embed)
+
+        def check(msg):
+            return msg.author == ctx.author and msg.channel == ctx.channel
+        try:
+            messageanswer = await self.client.wait_for('message', timeout=timehehe, check=check)
+            msgcontent = messageanswer.content
+
+            await self.initation.checkserver(ctx)
+            doc_ref = self.db.collection(u'users').document(
+                u'{}'.format(str(ctx.author.id)))
+            doc = doc_ref.get()
+            if doc.exists == False:
+                print("Nonexistant database")
+                return
+            dict1 = doc.to_dict()
+            if msgcontent == theanswer:
+                added = random.randint(1, 3)
+                embed = discord.Embed(
+                    title="Your answer " + theanswer + " was correct!",
+                    description=f"You are veery beeg brain! U gained {added}",
+                    color=self.client.primary_colour
+                )
+                dict1["money"] += added
+                await ctx.reply(embed=embed)
+
+            else:
+                embed = discord.Embed(
+                    title="<@!" + Your answer was wrong! The correct answer was " + theanswer,
+                    description=f"Not beeg brain :'( U lost 1 money!",
+                    color=self.client.primary_colour
+                )
+                dict1["money"] -= 1
+                if dict1["money"] < 0:
+                    dict1["money"] = 0
+                await ctx.reply(embed=embed)
+            doc_ref.set(dict1)
+        except asyncio.TimeoutError:
+            embed = discord.Embed(
+                title="You took too long. You math noob.",
+                description="How saddening",
+                color=self.client.primary_colour
+            )
+            await ctx.reply(embed=embed)
 
 
 def setup(client):
     client.add_cog(Quiz(client))
 
+
 def check(msg, ctx):
-        return msg.author == ctx.author and msg.channel == ctx.channel
+    return msg.author == ctx.author and msg.channel == ctx.channel
