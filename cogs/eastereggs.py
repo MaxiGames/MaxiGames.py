e611b17a55ac899b8f14fee441f7d3993dbe2f20
neon_paradox <glennwy.sun@gmail.com>
2021-07-17 17:22:38 +0800

    easteregg yayyyyyy
    first easteregg whoru or whoareu

---
+++ cogs/eastereggs.py
@@ -0,0 +1,40 @@
+import discord
+from discord.ext import commands
+# import tictactoe
+
+class EasterEggs(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+	
+    @commands.command()
+    async def whoru(self, ctx):
+        embed = discord.Embed(title = "Hey! Looks like you found this easter egg!",description = "Nice.",color = self.client.primary_colour)
+        embed.add_field(
+            name = "Whoami?",
+            value = "I am Maxigames! The creation of amateur bot devs <@!712942935129456671>, <@!676748194956181505> and <@!782247763542016010>",
+            inline = False
+        )
+        embed.add_field(
+            name = "Are there more easter eggs?",
+            value = "Yes ofc.",
+            inline = False
+        )
+        await ctx.author.send(embed=embed)
+    @commands.command
+    async def whoareu(self, ctx):
+        embed = discord.Embed(title = "Hey! Looks like you found this easter egg!",description = "Nice.",color = self.client.primary_colour)
+        embed.add_field(
+            name = "Whoami?",
+            value = "I am Maxigames! The creation of amateur bot devs <@!712942935129456671>, <@!676748194956181505> and <@!782247763542016010>",
+            inline = False
+        )
+        embed.add_field(
+            name = "Are there more easter eggs?",
+            value = "Yes ofc.",
+            inline = False
+        )
+        await ctx.author.send(embed=embed)
+
+
+def setup(client):
+    client.add_cog(EasterEggs(client))
