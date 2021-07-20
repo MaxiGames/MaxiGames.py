7dd6b0cce18030daf1c053fe2f29fbda3a37807c
Isaac <tux314159@gmail.com>
2021-07-20 14:27:50 +0800

    reindented starboard lol

--- cogs/starboard.py
+++ cogs/starboard.py
@@ -5,57 +5,57 @@ from firebase_admin import firestore
 from utils import check
 
 class Starboard(commands.Cog):
-		def __init__(self, client):
-				self.client = client
-				self.hidden = True
-				self.db = firestore.client()
-				self.initiation = self.client.get_cog("Initiation")
+    def __init__(self, client):
+        self.client = client
+        self.hidden = True
+        self.db = firestore.client()
+        self.initiation = self.client.get_cog("Initiation")
 
-		@check.is_admin()
-		@commands.command(name="starboard", description="Starts a starboard", usage="m!starboard #starboard")
-		async def starboard(self, ctx, channel: discord.TextChannel = None):
-			if channel == None:
-				await ctx.reply("You need to specify a channel")
-				return
-			try:
-				self.initation = self.client.get_cog("Initiation")
-				await self.initation.checkserver(ctx)
-				doc_ref = self.db.collection(u'servers').document(str(ctx.guild.id))
-				doc = doc_ref.get()
-				data = doc.to_dict()
-				data["starboard"] = {"channel": channel.id}
-				doc_ref.set(data)
-				await ctx.reply(f"Starboard channel has been set to {channel}!")
-			except:
-				await ctx.reply("That channel does not exist")
+    @check.is_admin()
+    @commands.command(name="starboard", description="Starts a starboard", usage="m!starboard #starboard")
+    async def starboard(self, ctx, channel: discord.TextChannel = None):
+        if channel == None:
+            await ctx.reply("You need to specify a channel")
+            return
+        try:
+            self.initation = self.client.get_cog("Initiation")
+            await self.initation.checkserver(ctx)
+            doc_ref = self.db.collection(u'servers').document(str(ctx.guild.id))
+            doc = doc_ref.get()
+            data = doc.to_dict()
+            data["starboard"] = {"channel": channel.id}
+            doc_ref.set(data)
+            await ctx.reply(f"Starboard channel has been set to {channel}!")
+        except:
+            await ctx.reply("That channel does not exist")
 
-		@commands.Cog.listener()
-		async def on_reaction_add(self, reaction, user):
-				print("WORKING")
-				self.initiation = self.client.get_cog("Initiation")
-				doc_ref = self.db.collection(u'servers').document(str(reaction.message.guild.id))
-				doc = doc_ref.get()
-				data = doc.to_dict()
-				channel = self.client.get_channel(int(data["starboard"]["channel"]))
-				await self.initiation.checkserver(reaction.message)
+    @commands.Cog.listener()
+    async def on_reaction_add(self, reaction, user):
+        print("WORKING")
+        self.initiation = self.client.get_cog("Initiation")
+        doc_ref = self.db.collection(u'servers').document(str(reaction.message.guild.id))
+        doc = doc_ref.get()
+        data = doc.to_dict()
+        channel = self.client.get_channel(int(data["starboard"]["channel"]))
+        await self.initiation.checkserver(reaction.message)
 
-				if "starboard" not in data:
-					print('b')
-					return
-				if channel is None:
-					print("a")
-					return
+        if "starboard" not in data:
+            print('b')
+            return
+        if channel is None:
+            print("a")
+            return
 
-				if reaction.count > 1:
-					if reaction.emoji == "⭐":
-						try:
-							msg = await channel.fetch_message(data["starboard"][str(reaction.message.id)])
-							await msg.edit(embed=discord.Embed(title=f"Starboard: {reaction.count}", description=reaction.message.content, color=0x00ff00).set_footer(text=f"React with {'🌟'} to star this message").set_author(name=reaction.message.author.name, icon_url=reaction.message.author.avatar_url))
-							doc_ref.set(data)
-						except KeyError:
-							message = await channel.send(embed=discord.Embed(title=f"Starboard: {reaction.count}", description=reaction.message.content, color=0x00ff00).set_footer(text=f"React with {'🌟'} to star this message").set_author(name=reaction.message.author.name, icon_url=reaction.message.author.avatar_url))
-							data["starboard"][str(reaction.message.id)] = message.id
-							doc_ref.set(data)
+        if reaction.count > 1:
+            if reaction.emoji == "⭐":
+                try:
+                    msg = await channel.fetch_message(data["starboard"][str(reaction.message.id)])
+                    await msg.edit(embed=discord.Embed(title=f"Starboard: {reaction.count}", description=reaction.message.content, color=0x00ff00).set_footer(text=f"React with {'🌟'} to star this message").set_author(name=reaction.message.author.name, icon_url=reaction.message.author.avatar_url))
+                    doc_ref.set(data)
+                except KeyError:
+                    message = await channel.send(embed=discord.Embed(title=f"Starboard: {reaction.count}", description=reaction.message.content, color=0x00ff00).set_footer(text=f"React with {'🌟'} to star this message").set_author(name=reaction.message.author.name, icon_url=reaction.message.author.avatar_url))
+                    data["starboard"][str(reaction.message.id)] = message.id
+                    doc_ref.set(data)
 
 
 def setup(client):
