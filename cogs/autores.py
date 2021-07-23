f657b844d6b8dab6263f21c6df585c97ebcd731d
nianny <xiaonianhe@hotmail.com>
2021-07-23 21:51:50 +0800

    hm

--- cogs/autores.py
+++ cogs/autores.py
@@ -1,1 +1,12 @@
 import discord
+from discord.ext import commands
+
+class Autoresponse(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    
+
+
+def setup(client):
+    client.add_cog(Autoresponse(client))
