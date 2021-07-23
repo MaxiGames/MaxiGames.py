995312cfcf6cdc72acd1ec4b613825094fb80ccf
Ang Jun Ray <angjunray@live.com>
2021-07-23 17:02:57 +0800

    Update case.py

--- cogs/case.py
+++ cogs/case.py
@@ -5,6 +5,7 @@ from discord.ext import commands
 class Case(commands.Cog):
 	def __init__(self, client):
 		self.client = client
+		self.hidden = False
 
 	@commands.command(name="snakecase", description="turns a bunch of words into snake case format", usage="snakecase <message>", alias=["sc"])
 	async def snakecase(self, ctx, *message):
