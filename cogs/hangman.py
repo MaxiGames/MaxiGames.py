cf6073d471be2b61c626da87cedc9b3c7374eede
Ang Jun Ray <angjunray@live.com>
2021-07-28 18:11:01 +0800

    trivia not rllly working yet tho

--- cogs/hangman.py
+++ cogs/hangman.py
@@ -13,9 +13,9 @@ import sys
 import asyncio
 import time
 from utils.paginator import Paginator
+from utils.leaderboard import leaderboard_generate
 import firebase_admin
 from firebase_admin import firestore
-
 from discord_components import Button, ButtonStyle
 
 alpha = "abcdefghijklmnopqrstuvwxyz"
@@ -299,70 +299,7 @@ class Hangman(commands.Cog):
     @commands.command(title="hangmanLB", description="The leaderboard for hangman", aliases=["hangmanleaderboard"])
     @cooldown(1, 15, BucketType.user)
     async def hangmanLB(self, ctx):
-        #! Hangman Firebase Initalisation
-        self.init = self.client.get_cog("Init")
-        await self.init.checkserver(ctx)
-        doc_ref = self.db.collection(u'users')
-        collection = doc_ref.stream()
-
-        userWinData = []
-        for doc in collection:
-            dictionary = doc.to_dict()
-            if "hangmanWins" in dictionary:
-                userWinData.append({"wins":dictionary["hangmanWins"], "name": await self.client.fetch_user(doc.id)})
-
-        userWinData = sorted(userWinData, key=lambda k: k['wins'], reverse=True)
-        #! PAGES
-        pages = []
-        page = discord.Embed(
-            title="Leaderboard!",
-            description="Hangman leaderboard",
-            colour=self.client.primary_colour
-        )
-        page.set_author(name=self.client.user.name,
-                        icon_url=self.client.user.avatar_url)
-        page.set_footer(text="Press Next to see the leaderboard :D")
-        pages.append(page)
-
-        length = len(userWinData)
-        total_pages = length//20 + 1
-        count = 0
-        count1 = 0
-        for i in range(0, total_pages):
-            count1 += 1
-            string = ""
-            for j in range(0, 20):
-                count += 1
-                if i*20 + j >= length:
-                    break
-                curList = userWinData[i*20+j]
-                name = curList['name'].name
-                disc = curList['name'].discriminator
-                wins = curList['wins']
-                string += f"#{count}. `{name}#{disc}`-`{wins}`\n"
-            
-            page = discord.Embed(
-                title=f"Page: {count1}",
-                description=string,
-                colour=self.client.primary_colour
-            )
-            page.set_author(name=self.client.user.name,
-                            icon_url=self.client.user.avatar_url)
-            pages.append(page)
-
-        page_num = 0
-        msg = await ctx.send(
-            embed=pages[page_num],
-        )
-        buttons = [[
-            Button(
-                style=ButtonStyle.URL,
-                label="Invite the bot!",
-                url="https://discord.com/api/oauth2/authorize?client_id=863419048041381920&permissions=8&scope=bot%20applications.commands",
-            )
-        ]]
-        page = Paginator(self.client, ctx, msg, pages,buttons=buttons ,timeout=120)
-        await page.start()
+        await leaderboard_generate(self, ctx, "hangman")
 
 
 def setup(client):
--- cogs/quiz.py
+++ cogs/quiz.py
@@ -3,7 +3,6 @@ from discord.ext import commands
 from discord.ext.commands import cooldown, BucketType
 import random
 import requests
-from discord_components import *
 import math
 import json
 import asyncio
@@ -13,8 +12,9 @@ from utils.paginator import Paginator
 import os
 import copy
 from utils import check
+from utils.leaderboard import leaderboard_generate
 import asyncio
-from firebase_admin import firestore
+from discord_components import Button, ButtonStyle
 
 alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
@@ -25,7 +25,6 @@ class Games(commands.Cog):
         self.init = self.client.get_cog("Init")
         self.db = firestore.client()
         self.hidden = False
-
     
     @commands.command(
         name="trivia",
@@ -116,6 +115,7 @@ class Games(commands.Cog):
             await ctx.reply(embed=embed)
 
         else:
+            # find which option is correct????
             index = None
             if reaction.emoji == "1️⃣":
                 index = 0
@@ -138,6 +138,9 @@ class Games(commands.Cog):
                 dict1 = doc.to_dict()
                 if ans == str(results["correct_answer"]):
                     dict1["money"] = dict1["money"] + random.randint(0, moneyToAdd)
+                    if "trivia" in dict1.keys():
+                        dict1["trivia"] = 0
+                    dict1["trivia"] += 1
                     embed = discord.Embed(
                         title="Correct Answer! You win 3 money!",
                         description="You now have " + str(dict1["money"]) + " money!",
@@ -148,13 +151,13 @@ class Games(commands.Cog):
                 else:
 
                     dict1["money"] = dict1["money"] - int(
-                        random.randint(0, moneyToAdd) / 3
+                        random.randint(1, moneyToAdd) // 3
                     )
                     embed = discord.Embed(
                         title="Wrong Answer! You lost 1 money!",
                         description=f"You now have "
                         + str(dict1["money"])
-                        + f" money! The correct answer was {results['correct_answer']}",
+                        + f" money! The correct answer was {results['correct_answer']} :",
                         color=self.client.primary_colour,
                     )
                     await ctx.reply(embed=embed)
@@ -163,70 +166,7 @@ class Games(commands.Cog):
     @commands.command(name="triviaLB", description="retrieves the trivia leaderboard", aliases=["trivialeaderboard"], usage="triviaLB")
     @cooldown(1, 10, BucketType.user)
     async def trivia_leaderboard(self, ctx):
-        self.init = self.client.get_cog("Init")
-        await self.init.checkserver(ctx)
-        doc_ref = self.db.collection(u'users')
-        collection = doc_ref.stream()
-
-        userWinData = []
-        for doc in collection:
-            dictionary = doc.to_dict()
-            if "triviaWins" in dictionary:
-                userWinData.append({"wins":dictionary["triviaWins"], "name": await self.client.fetch_user(doc.id)})
-
-        userWinData = sorted(userWinData, key=lambda k: k['wins'], reverse=True)
-        #! PAGES
-        pages = []
-        page = discord.Embed(
-            title="Leaderboard!",
-            description="Trivia leaderboard",
-            colour=self.client.primary_colour
-        )
-        page.set_author(name=self.client.user.name,
-                        icon_url=self.client.user.avatar_url)
-        page.set_footer(text="Press Next to see the leaderboard :D")
-        pages.append(page)
-
-        length = len(userWinData)
-        total_pages = length//20 + 1
-        count = 0
-        count1 = 0
-        for i in range(0, total_pages):
-            count1 += 1
-            string = ""
-            for j in range(0, 20):
-                count += 1
-                if i*20 + j >= length:
-                    break
-                curList = userWinData[i*20+j]
-                name = curList['name'].name
-                disc = curList['name'].discriminator
-                wins = curList['wins']
-                string += f"#{count}. `{name}#{disc}`-`{wins}`\n"
-            
-            page = discord.Embed(
-                title=f"Page: {count1}",
-                description=string,
-                colour=self.client.primary_colour
-            )
-            page.set_author(name=self.client.user.name,
-                            icon_url=self.client.user.avatar_url)
-            pages.append(page)
-
-        page_num = 0
-        msg = await ctx.send(
-            embed=pages[page_num],
-        )
-        #why is this here ?!?!
-        buttons = [[
-            Button(
-                style=ButtonStyle.URL,
-                label="Invite the bot!",
-                url="https://discord.com/api/oauth2/authorize?client_id=863419048041381920&permissions=8&scope=bot%20applications.commands",
-            )
-        ]]
-        page = Paginator(self.client, ctx, msg, pages,buttons=buttons ,timeout=120)
-        await page.start()
+        await leaderboard_generate(self, ctx, "trivia")
 
     @commands.command(
         name="math",
