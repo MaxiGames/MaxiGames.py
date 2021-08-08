0ea2bd20c334c684e5b25a88a7f4ea99a58df72b
Isaac <tux314159@gmail.com>
2021-08-09 00:34:42 +0800

    script to deploy to heroku ;)

---
+++ heroku-deploy.sh
@@ -0,0 +1,35 @@
+#! /usr/bin/sh
+
+if [ ! $(git branch --show-current) = "main" ]; then
+    echo "Can only deploy from branch main."
+    exit 1
+fi
+
+tar cf tar serviceAccountKey.json serviceAccountKey2.json config.json 
+s=$?
+bruh=0
+
+function cleanup()
+{
+    if [ $bruh -eq 0 ]; then
+        git reset --hard HEAD~
+        tar xf tar 2>&1 >/dev/null  # oh, shut up
+        rm -f tar
+        bruh=1
+    fi
+}
+
+trap cleanup SIGINT
+
+if [ $s -eq 0 ]; then
+    git add -f serviceAccountKey.json serviceAccountKey2.json config.json
+    s1=$?
+    git commit --allow-empty -m heroku
+
+    if [ $s1 -eq 0 ]; then
+        git push -f heroku main
+    else
+        echo 'Failed to add config files...'
+    fi
+    cleanup
+fi
