1f6c59e7fb3c3531f1262fee38c90c08f9376830
Isaac <tux314159@gmail.com>
2021-08-09 19:30:22 +0800

    confuzzlement

--- heroku-deploy.sh
+++ heroku-deploy.sh
@@ -1,14 +1,14 @@
 #! /bin/sh
 
+echo "Deploying from branch $(git branch --show-current) to Heroku."
+
 if [ ! $(git branch --show-current) = "main" ]; then
     echo "Can only deploy from branch main. Abort."
     exit 1
 fi
 
 rm -f tar
-echo "Archiving configs."
-tar cf tar serviceAccountKey.json serviceAccountKey2.json config.json 2>&1 >/dev/null  # shh
-echo "Archiving configs complete."
+tar cf tar serviceAccountKey.json serviceAccountKey2.json config.json >/dev/null 2>&1  # shh
 s=$?
 bruh=0  # bruh
 
@@ -16,8 +16,8 @@ function cleanup()
 {
     if [ $bruh -eq 0 ]; then
         echo "Cleaning up, unarchiving configs."
-        git reset --hard HEAD~ 2>&1 >/dev/null  # oh, shut up
-        tar xf tar 2>&1 >/dev/null  # you shut up too
+        git reset --hard HEAD~ >/dev/null 2>&1  # oh, shut up
+        tar xf tar >/dev/null 2>&1  # you shut up too
         rm -f tar
         echo "Cleaning up, unarchiving configs complete."
         bruh=1
@@ -31,13 +31,13 @@ if [ ! $s -eq 0 ]; then
     exit 1
 fi
 
-git add -f serviceAccountKey.json serviceAccountKey2.json config.json 2>&1 >/dev/null  # please just shut up
+git add -f serviceAccountKey.json serviceAccountKey2.json config.json >/dev/null 2>&1  # please just shut up
 s1=$?
-git commit --allow-empty -m heroku 2>&1 >/dev/null  # please just shut up as well
+git commit --allow-empty -m heroku >/dev/null 2>&1  # please just shut up as well
 
 if [ $s1 -eq 0 ]; then
-    heroku restart 2>&1 >/dev/null
-    heroku buildpacks:clear 2>&1 >/dev/null
+    heroku restart >/dev/null 2>&1 
+    heroku buildpacks:clear >/dev/null 2>&1 
     git push -fq heroku main | sed 's/^remote: //g'
 else
     echo 'Failed to add config files. Abort.'
