a421e6fb0fcf49a0318f56133bcbaeb6dbf42ff9
Isaac <tux314159@gmail.com>
2021-08-09 19:38:27 +0800

    stash before resetting

--- heroku-deploy.sh
+++ heroku-deploy.sh
@@ -17,7 +17,8 @@ function cleanup()
     if [ $bruh -eq 0 ]; then
         echo "Cleaning up."
         git reset --hard HEAD~ >/dev/null 2>&1  # oh, shut up
-        tar xf tar >/dev/null 2>&1  # you shut up too
+        git stash pop >/dev/null 2>&1  # you shut up too
+        tar xf tar >/dev/null 2>&1  # go away
         rm -f tar
         bruh=1
     fi
@@ -33,15 +34,16 @@ fi
 git add -f serviceAccountKey.json serviceAccountKey2.json config.json >/dev/null 2>&1  # please just shut up
 s1=$?
 git commit --allow-empty -m heroku >/dev/null 2>&1  # please just shut up as well
+git stash >/dev/null 2>&1
 
 if [ $s1 -eq 0 ]; then
     heroku restart >/dev/null 2>&1 
     heroku buildpacks:clear >/dev/null 2>&1 
     git push -fq heroku main 2>&1 | sed 's/^remote: //g'
+    cleanup
+    exit 0
 else
     echo 'Failed to add config files. Abort.'
     cleanup
     exit 1
 fi
-cleanup
-exit 0
