3ee071410a3f5dd47340a07ab48cee684179253a
nianny <xiaonianhe@hotmail.com>
2021-09-06 15:33:28 +0800

    hm

--- add.py
+++ add.py
@@ -2,7 +2,7 @@ import firebase_admin
 from firebase_admin import firestore
 from firebase_admin import credentials
 
-cred = credentials.Certificate("serviceAccountKey.json")
+cred = credentials.Certificate("serviceAccountKey2.json")
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
